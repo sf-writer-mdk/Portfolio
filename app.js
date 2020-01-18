81ff4c0a7d09c1d65d2d445548923706e55a43f2
Marvin Kirkland <sf.writer.mdk@gmail.com>
2020-01-18 10:35:46 -0800

    header background change

--- app.js
+++ app.js
@@ -21,23 +21,21 @@ $(window).on("scroll", function() {
             $(".header").removeClass("actives");
 
     } else if 
-    ($(window).scrollTop() > 1287 && $(window).scrollTop() < 2068) {
+    ($(window).scrollTop() > 1287 && $(window).scrollTop() < 1968) {
         $(".header").addClass("actives");
 
     } else if 
-    ($(window).scrollTop() > 2068 && $(window).scrollTop() < 3270) {
+    ($(window).scrollTop() > 1968 && $(window).scrollTop() < 3270) {
         $(".header").removeClass("actives");
 
     } else if 
     ($(window).scrollTop() > 3270 && $(window).scrollTop() < 6204) {
         $(".header").addClass("actives");
     
-
     } else {
         //remove the background property so it comes transparent again (defined in your css)
        $(".header").removeClass("actives");
     }
-    
 });
 
 
